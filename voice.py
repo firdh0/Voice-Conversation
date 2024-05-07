bfb7b19583b7fee5c8e19630818442aba70ae9cd
firdh0 <86157449+firdh0@users.noreply.github.com>
2024-05-07 12:32:48 +0700

    Update voice.py

--- voice.py
+++ voice.py
@@ -2,7 +2,6 @@
 import base64
 import os
 import uuid
-import requests
 
 import streamlit as st
 from openai import OpenAI
@@ -10,55 +9,22 @@ from openai import OpenAI
 class Voice:
 
     def __init__(self, api_key):
-        # self.client = OpenAI(api_key=api_key)
-        self.key = api_key
+        self.client = OpenAI(api_key=api_key)
         self.path = './chatbot/voice'
 
         if not os.path.exists(self.path):
             os.makedirs(self.path)
 
-    # def text_to_speech(self, transcript):
-    #     response = self.client.audio.speech.create(
-    #         model="tts-1-hd",
-    #         voice="echo",
-    #         input=transcript,
-    #     )
-
-    #     unique_filename = str(uuid.uuid4()) + ".mp3"
-    #     filepath = os.path.join(self.path, unique_filename)
-    #     response.stream_to_file(filepath)
-
-    #     return filepath
-
-    def text_to_speech(self, transcript, voice_id='VR6AewLTigWG4xSOukaG'):
-        tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
-
-        headers = {
-            "Accept": "application/json",
-            "xi-api-key": self.key
-        }
-
-        # Set up the data payload for the API request, including the text and voice settings
-        data = {
-            "text": transcript,
-            "model_id": "eleven_multilingual_v2",
-            "voice_settings": {
-                "stability": 0.5,
-                "similarity_boost": 0.9,
-                "style": 0.0,
-                "use_speaker_boost": True
-            }
-        }
-
-        response = requests.post(tts_url, headers=headers, json=data, stream=True)
+    def text_to_speech(self, transcript):
+        response = self.client.audio.speech.create(
+            model="tts-1-hd",
+            voice="echo",
+            input=transcript,
+        )
 
         unique_filename = str(uuid.uuid4()) + ".mp3"
         filepath = os.path.join(self.path, unique_filename)
-
-        with open(filepath, "wb") as f:
-        # Read the response in chunks and write to the file
-            for chunk in response.iter_content(chunk_size=1024):
-                f.write(chunk)
+        response.stream_to_file(filepath)
 
         return filepath
 
