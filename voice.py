1874304204f6e138c952c65f6140db4ff55ec72a
firdh0 <86157449+firdh0@users.noreply.github.com>
2024-05-10 16:59:13 +0700

    Update voice.py

--- voice.py
+++ voice.py
@@ -11,38 +11,37 @@ from elevenlabs.client import ElevenLabs
 class Voice:
 
     def __init__(self, api_key):
-        # self.client = OpenAI(api_key=api_key)
-        self.key = api_key
+        self.client = OpenAI(api_key=api_key)
         self.path = './chatbot/voice'
 
         if not os.path.exists(self.path):
             os.makedirs(self.path)
 
     def text_to_speech(self, transcript):
-        # response = self.client.audio.speech.create(
-        #     model="tts-1-hd",
-        #     voice="echo",
-        #     input=transcript,
-        # )
-
-        # unique_filename = str(uuid.uuid4()) + ".mp3"
-        # filepath = os.path.join(self.path, unique_filename)
-        # response.stream_to_file(filepath)
-
-        client = ElevenLabs(
-            api_key=self.key, # Defaults to ELEVEN_API_KEY
-        )
-
-        audio = client.generate(
-            text=transcript,
-            voice="Rachel",
-            model="eleven_multilingual_v2"
+        response = self.client.audio.speech.create(
+            model="tts-1-hd",
+            voice="echo",
+            input=transcript,
         )
 
         unique_filename = str(uuid.uuid4()) + ".mp3"
         filepath = os.path.join(self.path, unique_filename)
+        response.stream_to_file(filepath)
 
-        save(audio, filepath)
+        # client = ElevenLabs(
+        #     api_key=self.key, # Defaults to ELEVEN_API_KEY
+        # )
+
+        # audio = client.generate(
+        #     text=transcript,
+        #     voice="Rachel",
+        #     model="eleven_multilingual_v2"
+        # )
+
+        # unique_filename = str(uuid.uuid4()) + ".mp3"
+        # filepath = os.path.join(self.path, unique_filename)
+
+        # save(audio, filepath)
 
         return filepath
 
